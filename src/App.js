5838d67413d64211ebd3959e2c17dc980037e611
UrukuTelal <ao.bier1101@gmail.com>
2021-11-12 16:50:07 -0800

    not pretty form is up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,74 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
+/*
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+
+})
+*/
 
 const App = () => {
-  return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+  const [form, setForm] = useState({
+    size: '',
+    sauce: '',
+    toppings: [],
+    substitution: false,
+    instructions: '',
+    quantity: 0,
+    total: 0,
+  })
+
+  const [disabled, setDisabled] = useState(true)
+  
+  const change = event => {
+    const {checked, value, name, type} = event.target
+    const valueToUse = type === 'checkbox' ? checked : value
+    setForm({...form,[name]: valueToUse})
+  }
+
+  return (    
+      <div className='App'>
+      <form onSubmit>
+          <label>Size
+          <input 
+          onChange={change} 
+          value={form.size} 
+          name='size' type='dropdown' 
+          />
+          </label>Sauce
+          <label>
+          <input 
+          onChange={change} 
+          value={form.sauce} 
+          name='sauce' type='radial' 
+          />
+          </label>
+          <label>Toppings
+          <input 
+          onChange={change} 
+          value={form.toppings} 
+          name='toppings' type='checkbox' 
+          />
+          </label>
+          <label>Substitution
+          <input 
+          onChange={change} 
+          value={form.substitution} 
+          name='substitution' type='boolean' 
+          />
+          </label>
+          
+          <label>Special Instructions
+          <input 
+          onChange={change} 
+          value={form.instructions} 
+          name='instructions' type='text' 
+          />
+          </label>
+                  
+      
+      </form>
+      </div>
   );
 };
 export default App;
